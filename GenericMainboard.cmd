bef0627957439d0cea085242ff87f34401f2a495
mirage335 <spamfreemirage335@gmail.com>
2013-10-16 05:54:40 +0000

    first commit

---
+++ GenericMainboard.cmd
@@ -0,0 +1,271 @@
+# Pin name action command file
+
+# Start of element CONN4
+ChangePinName(CONN4, 1, 1)
+ChangePinName(CONN4, 2, 2)
+
+# Start of element Z12
+ChangePinName(Z12, 2, 2)
+ChangePinName(Z12, 1, 1)
+
+# Start of element R25
+ChangePinName(R25, 1, 1)
+ChangePinName(R25, 2, 2)
+
+# Start of element Z13
+ChangePinName(Z13, 2, 2)
+ChangePinName(Z13, 1, 1)
+
+# Start of element R24
+ChangePinName(R24, 1, 1)
+ChangePinName(R24, 2, 2)
+
+# Start of element R23
+ChangePinName(R23, 1, 1)
+ChangePinName(R23, 2, 2)
+
+# Start of element Z11
+ChangePinName(Z11, 2, 2)
+ChangePinName(Z11, 1, 1)
+
+# Start of element R22
+ChangePinName(R22, 1, 1)
+ChangePinName(R22, 2, 2)
+
+# Start of element Z10
+ChangePinName(Z10, 2, 2)
+ChangePinName(Z10, 1, 1)
+
+# Start of element R21
+ChangePinName(R21, 1, 1)
+ChangePinName(R21, 2, 2)
+
+# Start of element Z9
+ChangePinName(Z9, 2, 2)
+ChangePinName(Z9, 1, 1)
+
+# Start of element R20
+ChangePinName(R20, 1, 1)
+ChangePinName(R20, 2, 2)
+
+# Start of element Z8
+ChangePinName(Z8, 2, 2)
+ChangePinName(Z8, 1, 1)
+
+# Start of element Z6
+ChangePinName(Z6, 2, 2)
+ChangePinName(Z6, 1, 1)
+
+# Start of element R19
+ChangePinName(R19, 1, 1)
+ChangePinName(R19, 2, 2)
+
+# Start of element Z7
+ChangePinName(Z7, 2, 2)
+ChangePinName(Z7, 1, 1)
+
+# Start of element R18
+ChangePinName(R18, 1, 1)
+ChangePinName(R18, 2, 2)
+
+# Start of element Z4
+ChangePinName(Z4, 2, 2)
+ChangePinName(Z4, 1, 1)
+
+# Start of element R17
+ChangePinName(R17, 1, 1)
+ChangePinName(R17, 2, 2)
+
+# Start of element Z5
+ChangePinName(Z5, 2, 2)
+ChangePinName(Z5, 1, 1)
+
+# Start of element R16
+ChangePinName(R16, 1, 1)
+ChangePinName(R16, 2, 2)
+
+# Start of element F1
+ChangePinName(F1, 2, 2)
+ChangePinName(F1, 1, 1)
+
+# Start of element R15
+ChangePinName(R15, 1, 1)
+ChangePinName(R15, 2, 2)
+
+# Start of element Z3
+ChangePinName(Z3, 2, 2)
+ChangePinName(Z3, 1, 1)
+
+# Start of element R14
+ChangePinName(R14, 1, 1)
+ChangePinName(R14, 2, 2)
+
+# Start of element Z2
+ChangePinName(Z2, 2, 2)
+ChangePinName(Z2, 1, 1)
+
+# Start of element U5
+ChangePinName(U5, 3, OUT)
+ChangePinName(U5, 1, GND)
+ChangePinName(U5, 2, IN)
+
+# Start of element R13
+ChangePinName(R13, 1, 1)
+ChangePinName(R13, 2, 2)
+
+# Start of element Z1
+ChangePinName(Z1, 2, 2)
+ChangePinName(Z1, 1, 1)
+
+# Start of element CONN3
+ChangePinName(CONN3, 1, 1)
+ChangePinName(CONN3, 14, 14)
+ChangePinName(CONN3, 2, 2)
+ChangePinName(CONN3, 15, 15)
+ChangePinName(CONN3, 3, 3)
+ChangePinName(CONN3, 16, 16)
+ChangePinName(CONN3, 4, 4)
+ChangePinName(CONN3, 17, 17)
+ChangePinName(CONN3, 5, 5)
+ChangePinName(CONN3, 18, 18)
+ChangePinName(CONN3, 6, 6)
+ChangePinName(CONN3, 19, 19)
+ChangePinName(CONN3, 7, 7)
+ChangePinName(CONN3, 20, 20)
+ChangePinName(CONN3, 8, 8)
+ChangePinName(CONN3, 21, 21)
+ChangePinName(CONN3, 9, 9)
+ChangePinName(CONN3, 22, 22)
+ChangePinName(CONN3, 10, 10)
+ChangePinName(CONN3, 23, 23)
+ChangePinName(CONN3, 11, 11)
+ChangePinName(CONN3, 24, 24)
+ChangePinName(CONN3, 12, 12)
+ChangePinName(CONN3, 25, 25)
+ChangePinName(CONN3, 13, 13)
+
+# Start of element C4
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element C7
+ChangePinName(C7, 2, 2)
+ChangePinName(C7, 1, 1)
+
+# Start of element R12
+ChangePinName(R12, 1, 1)
+ChangePinName(R12, 2, 2)
+
+# Start of element D1
+ChangePinName(D1, 2, 2)
+ChangePinName(D1, 1, 1)
+
+# Start of element CONN1
+ChangePinName(CONN1, 5, 5)
+ChangePinName(CONN1, 3, 3)
+ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 6, 6)
+ChangePinName(CONN1, 4, 4)
+ChangePinName(CONN1, 2, 2)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element U4
+ChangePinName(U4, 1, 1)
+ChangePinName(U4, 2, 2)
+
+# Start of element U3
+ChangePinName(U3, 1, 1)
+ChangePinName(U3, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element CONN2
+ChangePinName(CONN2, 6, 6)
+ChangePinName(CONN2, 5, 5)
+ChangePinName(CONN2, 4, 4)
+ChangePinName(CONN2, 3, 3)
+ChangePinName(CONN2, 2, 2)
+ChangePinName(CONN2, 1, 1)
+
+# Start of element C5
+ChangePinName(C5, 2, 2)
+ChangePinName(C5, 1, 1)
+
+# Start of element C6
+ChangePinName(C6, 2, 2)
+ChangePinName(C6, 1, 1)
+
+# Start of element U2
+ChangePinName(U2, 2, 2)
+ChangePinName(U2, 1, 1)
+
+# Start of element R5
+ChangePinName(R5, 1, 1)
+ChangePinName(R5, 2, 2)
+
+# Start of element R6
+ChangePinName(R6, 1, 1)
+ChangePinName(R6, 2, 2)
+
+# Start of element U1
+ChangePinName(U1, 41, "PF0 (ADC0)")
+ChangePinName(U1, 40, "PF1 (ADC1)")
+ChangePinName(U1, 39, "PF4 (ADC4/TCK)")
+ChangePinName(U1, 38, "PF5 (ADC5/TMS)")
+ChangePinName(U1, 37, "PF6 (ADC6/TDO)")
+ChangePinName(U1, 36, "PF7 (ADC7/TDI)")
+ChangePinName(U1, 18, "PD0 (OC0B/SCL/INT0)")
+ChangePinName(U1, 19, "PD1 (SDA/INT1)")
+ChangePinName(U1, 20, "PD2 (RXD1/INT2)")
+ChangePinName(U1, 21, "PD3 (TXD1/INT3)")
+ChangePinName(U1, 25, "PD4 (ICP1/ADC8)")
+ChangePinName(U1, 22, "PD5 (XCK1/CTS)")
+ChangePinName(U1, 26, "PD6 (T1/OC4D/ADC9)")
+ChangePinName(U1, 27, "PD7 (T0/OC4D/ADC10)")
+ChangePinName(U1, 33, "PE2 (HWB)")
+ChangePinName(U1, 1, "PE6 (INT.6/AIN0)")
+ChangePinName(U1, 31, "PC6 (OC3A/OC4A)")
+ChangePinName(U1, 32, "PC7 (ICP3/CLK0/OC4A)")
+ChangePinName(U1, 8, "PB0 (SS/PCINT0)")
+ChangePinName(U1, 9, "PB1 (PCINT1/SCLK)")
+ChangePinName(U1, 10, "PB2 (PDI/PCINT2/MOSI)")
+ChangePinName(U1, 11, "PB3 (PDO/PCINT3/MISO)")
+ChangePinName(U1, 28, "PB4 (PCINT4/ADC11)")
+ChangePinName(U1, 29, "PB5 (PCINT5/OC1A/OC4B/ADC12)")
+ChangePinName(U1, 30, "PB6 (PCINT6/OC1B/OC4B/ADC13)")
+ChangePinName(U1, 12, "PB7 (PCINT7/OC0A/OC1C/RTS)")
+ChangePinName(U1, 7, VBus)
+ChangePinName(U1, 5, UGnd)
+ChangePinName(U1, 4, D+)
+ChangePinName(U1, 3, D-)
+ChangePinName(U1, 2, UVcc)
+ChangePinName(U1, 6, UCap)
+ChangePinName(U1, 35, GND)
+ChangePinName(U1, 23, GND)
+ChangePinName(U1, 15, GND)
+ChangePinName(U1, 14, VCC)
+ChangePinName(U1, 42, AREF)
+ChangePinName(U1, 24, AVCC)
+ChangePinName(U1, 44, AVCC)
+ChangePinName(U1, 17, XTAL1)
+ChangePinName(U1, 16, XTAL2)
+ChangePinName(U1, 13, RESET)
+ChangePinName(U1, 43, GND)
+ChangePinName(U1, 34, VCC)
---
+++ gedasymbols/footprints/DO214AC.fp
@@ -0,0 +1,39 @@
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Footprint: DO214AC.fp</title>
+<link rel="stylesheet" href="/gedasymbols.css" type="text/css">
+</head>
+<body>
+<h1 align=center>Footprint: DO214AC.fp</h1>
+
+<p>
+File size: 354 bytes <br>
+File date: Fri Dec 17 09:26:25 2010 <br>
+<a href="DO214AC.fp?view">View</a> or 
+<a href="DO214AC.fp?dl">Download</a>
+(<a href="DO214AC.fp?dlattr">with added source attribution</a>)
+</p>
+
+<p>
+</p>
+
+
+<p>
+<img src="DO214AC.fp?png">
+</p>
+<p><br></p>
+<p><hr noshade size=1></p>
+
+<small>
+
+<p align=center>
+
+Copyright 2010, all rights reserved unless specified otherwise.
+<br>
+<a href="/">gedasymbols.org</a> is maintained by
+<a href="/user/dj_delorie/">DJ Delorie</a></p>
+
+</small>
+
+</body>
+</html>
